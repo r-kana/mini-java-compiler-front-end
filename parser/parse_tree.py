951a9ed96b5b4def4fc4b728c8526fdc33fb1860
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2025-01-11 13:57:12 -0300

    adicao de atributo e correcao de retorno de funcao

--- parser/parse_tree.py
+++ parser/parse_tree.py
@@ -1,9 +1,10 @@
 class TreeNode:
-  def __init__(self, parent=None, token=''):
+  def __init__(self, parent=None, token='', lexeme=None):
     self.parent = parent
     self.children = []
     self.token = token
     self.children_count = 0
+    self.lexeme = lexeme
     
     
   def append_children(self, production):
@@ -11,6 +12,6 @@ class TreeNode:
     
 
   def is_complete(self):
-    self.children_count == len(self.children)
+    return self.children_count == len(self.children)
     
   
