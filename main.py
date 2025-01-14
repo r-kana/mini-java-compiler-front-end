e1353e3f5e360576e4abc7347426bd8a116e1c19
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2025-01-14 14:43:52 -0300

    atualizando repo Co-authored-by: Luca-Machado-uff <Luca-Machado-uff@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from Code_Gen.code_gen import code_gen
 from Parser.parser import parser, print_parse_tree, ROOT
 from Scanner.minijavaplus import MiniJava
+from Scanner.token import Token
 
-parser(MiniJava.main('./example.java'))
 
-print_parse_tree(ROOT)
+token: list[Token]
+tokens = MiniJava.main('./example.java')
+
+parser(tokens)
+
+# print_parse_tree(ROOT)
+
+code_gen(ROOT)
