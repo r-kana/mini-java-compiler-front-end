55d1e2fb02e932344928fa73d20000e66d05bb22
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2025-01-19 23:08:22 -0300

    adicionando chamanda para code_gen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Code_Gen.code_gen import code_gen
+from Code_Gen.code_gen import code_gen, print_ast
 from Parser.parser import parser, print_parse_tree, ROOT
 from Scanner.minijavaplus import MiniJava
 from Scanner.token import Token
@@ -10,5 +10,8 @@ tokens = MiniJava.main('./example.java')
 parser(tokens)
 
 # print_parse_tree(ROOT)
-
+# print('AST:\n')
+# print_ast(ROOT)
+# print('\n')
+print('Code Gen:\n')
 code_gen(ROOT)
